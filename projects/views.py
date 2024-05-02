5b6ed788257fb394efb9da7665d8e7711c9a6b04
avnms <sarmaakondivnm@icloud.com>
2024-05-02 11:10:30 +1000

    created and configured sample templates

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our projects.")
+    return render(request, "projects/projects.html")
 
 
 def project(request, pk):
-    return HttpResponse(f"Single project {pk}")
+    return render(request, "projects/single-project.html")
